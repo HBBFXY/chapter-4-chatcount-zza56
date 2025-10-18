609ce88641c0418b869188d620cc7039a2094947
zza56 <1514326359@qq.com>
2025-10-18 18:58:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-letters = 0digits = 0spaces = 0others = 0
-line = input()
-for char in line:if char.isalpha():letters += 1elif char.isdigit():digits += 1elif char.isspace():spaces += 1else:others += 1
-print (f"英文字符: {letters}")print (f"数字: {digits}")print (f"空格: {spaces}")print (f"其他字符: {others}")
+letters = 0
+digits = 0
+spaces = 0
+others = 0
+
+input_str = input()
+
+for char in input_str:
+    if char.isalpha():
+        letters += 1
+    elif char.isdigit():
+        digits += 1
+    elif char.isspace():
+        spaces += 1
+    else:
+        others += 1
+
+print(f"英文字符: {letters}")
+print(f"数字: {digits}")
+print(f"空格: {spaces}")
+print(f"其他字符: {others}")
